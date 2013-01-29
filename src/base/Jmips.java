b83360d6af4380a86af2c8d1edd956fe7b9fb4a3
flvsilva <flv.silva@yahoo.com.br>
2013-01-29 02:59:37 -0200

    Criando um pacote para o processador

--- src/base/Jmips.java
+++ src/base/Jmips.java
@@ -1,7 +1,6 @@
-import java.io.DataInputStream;
+package base;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
